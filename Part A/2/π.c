3fb35f99add42072a2799c09ff470c59b7c9fa2c
Reehan <92086803+Reehan9@users.noreply.github.com>
2023-10-04 19:13:08 +0530

    Update π.c

--- Part A/2/π.c
+++ Part A/2/π.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
 #include <omp.h>
 
-#define NUM_THREADS 8
-static long num_steps = 10000000;
-double step;
+#define NUM_STEPS 10000000  // Number of steps/iterations for the approximation
 
 int main() {
-    int i;
-    double pi;
-    double sum[NUM_THREADS];
-    step = 1.0/(double) num_steps;
+    double pi = 0.0;
 
-    double start  = omp_get_wtime();
-    double sum_serial = 0.0;
-    for(i = 0; i < num_steps; i++){
-        double x = (i+0.5)*step;
-        sum_serial += 4.0/(1.0+x*x);
+    // Serial computation
+    double start_time = omp_get_wtime();
+    for (int k = 0; k < NUM_STEPS; k++) {
+        pi += (4.0 * (k % 2 == 0 ? 1 : -1)) / (2 * k + 1);
     }
-    pi = sum_serial / num_steps;
-    double end  = omp_get_wtime();
+    double end_time = omp_get_wtime();
+    printf("Serial version: pi = %6.12f, time = %f seconds\n", pi, end_time - start_time);
+
+    // Resetting pi for parallel computation
+    pi = 0.0;
     
-    printf("Serial version: pi = %6.12f, time = %f seconds\n", pi, end-start);
-
-    start  = omp_get_wtime();
-    #pragma omp parallel private(i) num_threads(NUM_THREADS)
-    {
-        int id = omp_get_thread_num();
-        for(i = id, sum[id] = 0.0; i < num_steps; i += NUM_THREADS){
-            double x = (i+0.5)*step;
-            sum[id] += 4.0/(1.0+x*x);
-        }
+    // Parallel computation
+    double sum[NUM_STEPS];
+    start_time = omp_get_wtime();
+    #pragma omp parallel for
+    for (int k = 0; k < NUM_STEPS; k++) {
+        sum[k] = (4.0 * (k % 2 == 0 ? 1 : -1)) / (2 * k + 1);
     }
-    for(i = 1; i < NUM_THREADS; i++) 
-        sum[0] += sum[i];
-    pi = sum[0] / num_steps;
-    end  = omp_get_wtime();
+    for (int k = 0; k < NUM_STEPS; k++) {
+        pi += sum[k];
+    }
+    end_time = omp_get_wtime();
+    printf("Parallel version: pi = %6.12f, time = %f seconds\n", pi, end_time - start_time);
 
-    printf("Parallel version: pi = %6.12f, time = %f seconds\n", pi, end-start);
     return 0;
 }

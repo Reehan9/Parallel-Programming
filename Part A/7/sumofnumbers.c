58cae10cfb1c084f3c95516376fb2cdcf0efae12
Reehan <92086803+Reehan9@users.noreply.github.com>
2023-10-04 20:28:21 +0530

    Update sumofnumbers.c

--- Part A/7/sumofnumbers.c
+++ Part A/7/sumofnumbers.c
@@ -1,10 +1,14 @@
 #include<stdio.h> 
 #include<stdlib.h> 
 #include<omp.h>
-void main(){
-int n =100;
-int sum = 0; 
-for(int i=1;i<=n;i++){
-#pragma omp critical
-sum+=i; }
-printf("sum is %d\n",sum); }
+
+  void main()  {
+        int n =100;
+        int sum = 0; 
+for(int i=1;i<=n;i++)  {
+          #pragma omp critical
+            sum+=i;
+      }
+printf("sum is %d\n",sum);
+  
+  }

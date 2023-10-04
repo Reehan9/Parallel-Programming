fdd74ceefccf06efd19e1205e9cce482ddd55323
Reehan <92086803+Reehan9@users.noreply.github.com>
2023-10-04 19:00:22 +0530

    Update prime_numbers.c

--- Part A/5/prime_numbers.c
+++ Part A/5/prime_numbers.c
@@ -18,19 +18,18 @@ int main() {
     int prime_count;
 
     // Serial version
-    clock_t start_time = clock();
+    double start  = omp_get_wtime();
     prime_count = 0;
     for (int i = 2; i <= n; i++) {
         if (is_prime(i)) {
             prime_count++;
         }
     }
-    clock_t end_time = clock();
-    double serial_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Serial version: Primes found = %d, time = %f seconds\n", prime_count, serial_time);
+    double end  = omp_get_wtime();
+    printf("Serial version: Primes found = %d, time = %f seconds\n", prime_count, end-start);
 
     // Parallel version
-    start_time = clock();
+    start  = omp_get_wtime();
     prime_count = 0;
     #pragma omp parallel for reduction(+:prime_count) schedule(dynamic)
     for (int i = 2; i <= n; i++) {
@@ -38,9 +37,9 @@ int main() {
             prime_count++;
         }
     }
-    end_time = clock();
-    double parallel_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Parallel version: Primes found = %d, time = %f seconds\n", prime_count, parallel_time);
+    end  = omp_get_wtime();
+    
+    printf("Parallel version: Primes found = %d, time = %f seconds\n", prime_count, end-start);
 
     return 0;
 }

cd43ba65e2fdc3ac236ddf14dd112cd01ef915b9
Reehan <92086803+Reehan9@users.noreply.github.com>
2023-10-04 19:03:56 +0530

    Update π.c

--- Part A/2/π.c
+++ Part A/2/π.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <omp.h>
-#include <time.h>
 
 #define NUM_THREADS 8
 static long num_steps = 10000000;
@@ -12,18 +11,18 @@ int main() {
     double sum[NUM_THREADS];
     step = 1.0/(double) num_steps;
 
-    clock_t start_time = clock();
+    double start  = omp_get_wtime();
     double sum_serial = 0.0;
     for(i = 0; i < num_steps; i++){
         double x = (i+0.5)*step;
         sum_serial += 4.0/(1.0+x*x);
     }
     pi = sum_serial / num_steps;
-    clock_t end_time = clock();
-    double serial_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Serial version: pi = %6.12f, time = %f seconds\n", pi, serial_time);
+    double end  = omp_get_wtime();
+    
+    printf("Serial version: pi = %6.12f, time = %f seconds\n", pi, end-start);
 
-    start_time = clock();
+    start  = omp_get_wtime();
     #pragma omp parallel private(i) num_threads(NUM_THREADS)
     {
         int id = omp_get_thread_num();
@@ -35,8 +34,8 @@ int main() {
     for(i = 1; i < NUM_THREADS; i++) 
         sum[0] += sum[i];
     pi = sum[0] / num_steps;
-    end_time = clock();
-    double parallel_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Parallel version: pi = %6.12f, time = %f seconds\n", pi, parallel_time);
+    end  = omp_get_wtime();
+
+    printf("Parallel version: pi = %6.12f, time = %f seconds\n", pi, end-start);
     return 0;
 }

73226df31e897fe2011b591858fd0878fed3a233
Reehan <syedreehan998@gmail.com>
2023-11-12 09:09:30 +0530

    Update

--- Part B/1/1.c
+++ Part B/1/1.c
@@ -1,29 +1,24 @@
 #include <mpi.h>
 #include <stdio.h>
-#include <string.h>
 
-int main(int argc, char** argv) {
-    MPI_Init(&argc, &argv);
+int main() {
+    MPI_Init(NULL, NULL);
 
-    int world_rank;
+    int world_rank, world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     if (world_size < 2) {
-        printf("The number of preocessors should be atleast 2\n");
+        printf("The number of processors should be at least 2\n");
         MPI_Finalize();
         return 0;
     }
 
-    int number;
+    int number = 7;
     if (world_rank == 0) {
-        // If we are rank 0, set the number to 7 and send it to rank 1
-        number = 7;
         MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
         printf("Process 0 sent number %d to process 1\n", number);
-    } else if (world_rank == 1) {
-        // If we are rank 1, receive the number from rank 0
+    } else {
         MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Process 1 received number %d from process 0\n", number);
     }

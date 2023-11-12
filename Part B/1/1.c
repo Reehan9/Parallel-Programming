ce402a5f4b2d2431a7cf078a8858706e8a3537e1
Reehan <syedreehan998@gmail.com>
2023-11-12 15:31:19 +0530

    Update

--- Part B/1/1.c
+++ Part B/1/1.c
@@ -25,3 +25,4 @@ int main() {
 
     MPI_Finalize();
     return 0;
+}
--- Part B/2/2.c
+++ Part B/2/2.c
@@ -9,14 +9,12 @@ int main(int argc, char *argv[]) {
 
     int number;
     if (world_rank == 0) {
-        // Process 0 sends a number to Process 1 and waits to receive from Process 1
         number = 0;
-        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Ssend(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
         MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     } else if (world_rank == 1) {
-        // Process 1 sends a number to Process 0 and waits to receive from Process 0
         number = 1;
-        MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Ssend(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
         MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 

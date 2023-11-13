0e59fcde41945dbc6a24f43113ce2410e21ea8bf
Reehan <syedreehan998@gmail.com>
2023-11-13 13:48:21 +0530

    Updated Programs

--- Part B/5/5.c
+++ Part B/5/5.c
@@ -8,14 +8,24 @@ int main() {
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // Phase 1: Each process performs a task (e.g., prints its rank)
-    printf("Phase 1, Process %d of %d\n", world_rank, world_size);
+    // Ensure there are at least two processes
+    if (world_size < 2) {
+        fprintf(stderr, "World size must be greater than 1\n");
+        MPI_Finalize();
+        return 1;
+    }
 
-    // Synchronize all processes before moving to the next phase
-    MPI_Barrier(MPI_COMM_WORLD);
+    int send_number = world_rank;
+    int recv_number;
 
-    // Phase 2: Another task after synchronization
-    printf("Phase 2, Process %d of %d\n", world_rank, world_size);
+    // Determine the rank of the target process
+    int target_rank = (world_rank + 1) % world_size;
+
+    MPI_Sendrecv(&send_number, 1, MPI_INT, target_rank, 0, 
+                 &recv_number, 1, MPI_INT, (world_rank + world_size - 1) % world_size, 0, 
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    printf("Process %d sent %d and received %d\n", world_rank, send_number, recv_number);
 
     MPI_Finalize();
     return 0;
